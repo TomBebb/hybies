6933582b5bb16e6b01bb3e6b1963d1082fb23721
Tom D Bebbington <tombebb@protonmail.com>
2026-02-08 21:43:01 +0000

    Use kotlin, fix package name

--- src/main/java/org/tomb/zombies/ZombiesPlugin.kt
+++ src/main/java/org/tomb/zombies/ZombiesPlugin.kt
@@ -1,27 +1,26 @@
-package org.example.plugin;
+package org.tomb.zombies
 
-import com.hypixel.hytale.logger.HytaleLogger;
-import com.hypixel.hytale.server.core.plugin.JavaPlugin;
-import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
-
-import javax.annotation.Nonnull;
+import com.hypixel.hytale.logger.HytaleLogger
+import com.hypixel.hytale.server.core.plugin.JavaPlugin
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit
+import javax.annotation.Nonnull
 
 /**
  * This class serves as the entrypoint for your plugin. Use the setup method to register into game registries or add
  * event listeners.
  */
-public class ExamplePlugin extends JavaPlugin {
-
-    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
-
-    public ExamplePlugin(@Nonnull JavaPluginInit init) {
-        super(init);
-        LOGGER.atInfo().log("Hello from " + this.getName() + " version " + this.getManifest().getVersion().toString());
+class ZombiesPlugin(@Nonnull init: JavaPluginInit) : JavaPlugin(init) {
+    init {
+        LOGGER.atInfo().log("Hello from ${this.name} version ${this.manifest.version}")
     }
 
-    @Override
-    protected void setup() {
-        LOGGER.atInfo().log("Setting up plugin " + this.getName());
-        this.getCommandRegistry().registerCommand(new ExampleCommand(this.getName(), this.getManifest().getVersion().toString()));
+    override fun setup() {
+        LOGGER.atInfo().log("Setting up plugin ${this.name}")
+        this.commandRegistry
+            .registerCommand(ZombiesCommand(this.name, this.manifest.version.toString()))
+    }
+
+    companion object {
+        private val LOGGER = HytaleLogger.forEnclosingClass()
     }
 }
